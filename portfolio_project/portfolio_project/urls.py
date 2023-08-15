7c50054d2339578d80c18dedd1a474b0cd80db80
Nikita <109899745+vvetta@users.noreply.github.com>
2023-08-15 22:52:16 +0300

    add index view

--- portfolio_project/portfolio_project/urls.py
+++ portfolio_project/portfolio_project/urls.py
@@ -17,6 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.contrib import admin
+from django.urls import path, include
+
+app_name = 'home'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('home.urls')),
 ]
